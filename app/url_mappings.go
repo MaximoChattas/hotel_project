3a462a86083a4c8ed9149315b1b7c69688828c02
MaximoChattas <maxichattas@gmail.com>
2023-05-17 12:47:11 -0300

    App directory created

---
+++ app/url_mappings.go
@@ -0,0 +1,14 @@
+package app
+
+import (
+	//Add necessary controllers here !
+
+	log "github.com/sirupsen/logrus"
+)
+
+func mapUrls() {
+
+	// Add all methods and its mappings
+
+	log.Info("Finishing mappings configurations")
+}
