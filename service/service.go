e44bc16ec305c70a51f338c776ca7cf3ab7bb3d0
MaximoChattas <maxichattas@gmail.com>
2023-05-21 17:47:36 -0300

    Service functions added

--- service/service.go
+++ service/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"project/dto"
+	"time"
 )
 
 type service struct{}
@@ -10,9 +11,17 @@ type serviceInterface interface {
 	GetUserById(id int) (dto.UserDto, error)
 	GetUsers() (dto.UsersDto, error)
 	InsertUser(userDto dto.UserDto) (dto.UserDto, error)
+
 	GetHotelById(id int) (dto.HotelDto, error)
 	GetHotels() (dto.HotelsDto, error)
 	InsertHotel(hotelDto dto.HotelDto) (dto.HotelDto, error)
+
+	InsertReservation(reservationDto dto.ReservationDto) (dto.ReservationDto, error)
+	GetReservations() (dto.ReservationsDto, error)
+	GetReservationsByUser(userId int) (dto.ReservationsDto, error)
+	GetReservationsByHotel(hotelId int) (dto.ReservationsDto, error)
+
+	CheckAvailability(hotelId int, startDate time.Time, endDate time.Time) bool
 }
 
 var (
