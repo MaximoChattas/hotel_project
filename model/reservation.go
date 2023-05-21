2c9ded0a7a650860853c03d9b1cd4b38bcd132d9
MaximoChattas <maxichattas@gmail.com>
2023-05-21 16:23:03 -0300

    CHange in time data type

--- model/reservation.go
+++ model/reservation.go
@@ -1,13 +1,11 @@
 package model
 
-import "time"
-
 type Reservation struct {
-	Id        int       `gorm:"primaryKey"`
-	StartDate time.Time `gorm:"column:StartDate"`
-	EndDate   time.Time `gorm:"column:EndDate"`
-	UserId    int       `gorm:"foreignkey:UserId"`
-	HotelId   int       `gorm:"foreignkey:HotelId"`
+	Id        int    `gorm:"primaryKey"`
+	StartDate string `gorm:"type:varchar(19); not null"`
+	EndDate   string `gorm:"type:varchar(19); not null"`
+	UserId    int    `gorm:"foreignkey:UserId"`
+	HotelId   int    `gorm:"foreignkey:HotelId"`
 }
 
 type Reservations []Reservation
