bbbdda79f91b4d81b3e94fab5e745d0a45edda7d
MaximoChattas <maxichattas@gmail.com>
2023-05-23 16:00:34 -0300

    Service merge

--- service/service.go
+++ service/service.go
@@ -11,9 +11,9 @@ import (
 type service struct{}
 
 type serviceInterface interface {
+	InsertUser(userDto dto.UserDto) (dto.UserDto, error)
 	GetUserById(id int) (dto.UserDto, error)
 	GetUsers() (dto.UsersDto, error)
-	InsertUser(userDto dto.UserDto) (dto.UserDto, error)
 
 	GetHotelById(id int) (dto.HotelDto, error)
 	GetHotels() (dto.HotelsDto, error)
@@ -21,10 +21,10 @@ type serviceInterface interface {
 
 	InsertReservation(reservationDto dto.ReservationDto) (dto.ReservationDto, error)
 	GetReservations() (dto.ReservationsDto, error)
-	GetReservationsByUser(userId int) (dto.ReservationsDto, error)
-	GetReservationsByHotel(hotelId int) (dto.ReservationsDto, error)
+	GetReservationsByUser(userId int) (dto.ReservationsDto, error)   //To do
+	GetReservationsByHotel(hotelId int) (dto.ReservationsDto, error) //To do
 
-	CheckAvailability(hotelId int, startDate time.Time, endDate time.Time) bool
+	CheckAvailability(hotelId int, startDate time.Time, endDate time.Time) bool //To do
 }
 
 var (
@@ -33,6 +33,61 @@ var (
 
 func init() {
 	Service = &service{}
+}
+
+func (s *service) InsertUser(userDto dto.UserDto) (dto.UserDto, error) {
+	var user model.User
+
+	user.Name = userDto.Name
+	user.LastName = userDto.LastName
+	user.Dni = userDto.Dni
+	user.Email = userDto.Email
+	user.Password = userDto.Password
+
+	user = client.InsertUser(user)
+
+	userDto.Id = user.Id
+
+	return userDto, nil
+}
+
+func (s *service) GetUserById(id int) (dto.UserDto, error) {
+
+	var user model.User = client.GetUserById(id)
+
+	var userDto dto.UserDto
+
+	if user.Id == 0 {
+		return userDto, errors.New("User not found")
+	}
+
+	userDto.Id = user.Id
+	userDto.Name = user.Name
+	userDto.LastName = user.LastName
+	userDto.Dni = user.Dni
+	userDto.Email = user.Email
+	userDto.Password = user.Password
+
+	return userDto, nil
+}
+
+func (s *service) GetUsers() (dto.UsersDto, error) {
+	var users model.Users = client.GetUsers()
+	var usersDto dto.UsersDto
+
+	for _, user := range users {
+		var userDto dto.UserDto
+		userDto.Id = user.Id
+		userDto.Name = user.Name
+		userDto.LastName = user.LastName
+		userDto.Dni = user.Dni
+		userDto.Email = user.Email
+		userDto.Password = user.Password
+
+		usersDto = append(usersDto, userDto)
+	}
+
+	return usersDto, nil
 }
 
 func (s *service) InsertHotel(hotelDto dto.HotelDto) (dto.HotelDto, error) {
@@ -49,7 +104,7 @@ func (s *service) InsertHotel(hotelDto dto.HotelDto) (dto.HotelDto, error) {
 	return hotelDto, nil
 }
 
-func (s *hotelService) GetHotel() (dto.HotelsDto, error) {
+func (s *service) GetHotels() (dto.HotelsDto, error) {
 
 	var hotels model.Hotels = client.GetHotels()
 	var hotelsDto dto.HotelsDto
@@ -70,7 +125,7 @@ func (s *hotelService) GetHotel() (dto.HotelsDto, error) {
 	return hotelsDto, nil
 }
 
-func (s *hotelService) GetHotelById(id int) (dto.HotelDto, error) {
+func (s *service) GetHotelById(id int) (dto.HotelDto, error) {
 
 	var hotel model.Hotel = client.GetHotelById(id)
 	var hotelDto dto.HotelDto
@@ -86,4 +141,39 @@ func (s *hotelService) GetHotelById(id int) (dto.HotelDto, error) {
 	hotelDto.StreetNumber = hotel.StreetNumber
 
 	return hotelDto, nil
+}
+
+func (s *service) InsertReservation(reservationDto dto.ReservationDto) (dto.ReservationDto, error) {
+	var reservation model.Reservation
+
+	reservation.StartDate = reservationDto.StartDate
+	reservation.EndDate = reservationDto.EndDate
+	reservation.HotelId = reservationDto.HotelId
+	reservation.UserId = reservationDto.UserId
+
+	reservation = client.InsertReservation(reservation)
+
+	reservationDto.Id = reservation.Id
+
+	return reservationDto, nil
+}
+
+func (s *service) GetReservations() (dto.ReservationsDto, error) {
+
+	var reservations model.Reservations = client.GetReservations()
+	var reservationsDto dto.ReservationsDto
+
+	for _, reservation := range reservations {
+		var reservationDto dto.ReservationDto
+
+		reservationDto.Id = reservation.Id
+		reservationDto.StartDate = reservation.StartDate
+		reservationDto.EndDate = reservation.EndDate
+		reservationDto.HotelId = reservation.HotelId
+		reservationDto.UserId = reservation.UserId
+
+		reservationsDto = append(reservationsDto, reservationDto)
+	}
+
+	return reservationsDto, nil
 }
