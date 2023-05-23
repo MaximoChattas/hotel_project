787c07e5dcce11196ec4fa31d19f6b926790dcd6
YagoGandara <yago@gandara.com.ar>
2023-05-23 15:10:39 -0300

    Funciones respectivas a hotel agregadas en el service

--- service/service.go
+++ service/service.go
@@ -1,7 +1,10 @@
 package service
 
 import (
+	"errors"
+	"project/client"
 	"project/dto"
+	"project/model"
 	"time"
 )
 
@@ -30,4 +33,57 @@ var (
 
 func init() {
 	Service = &service{}
+}
+
+func (s *service) InsertHotel(hotelDto dto.HotelDto) (dto.HotelDto, error) {
+	var hotel model.Hotel
+
+	hotel.Name = hotelDto.Name
+	hotel.Description = hotelDto.Description
+	hotel.RoomAmount = hotelDto.RoomAmount
+
+	hotel = client.InsertHotel(hotel)
+
+	hotelDto.Id = hotel.Id
+
+	return hotelDto, nil
+}
+
+func (s *hotelService) GetHotel() (dto.HotelsDto, error) {
+
+	var hotels model.Hotels = client.GetHotels()
+	var hotelsDto dto.HotelsDto
+
+	for _, hotel := range hotels {
+		var hotelDto dto.HotelDto
+		hotelDto.Id = hotel.Id
+		hotelDto.Name = hotel.Name
+		hotelDto.RoomAmount = hotel.RoomAmount
+		hotelDto.Description = hotel.Description
+
+		hotelDto.StreetName = hotel.StreetName
+		hotelDto.StreetNumber = hotel.StreetNumber
+
+		hotelsDto = append(hotelsDto, hotelDto)
+	}
+
+	return hotelsDto, nil
+}
+
+func (s *hotelService) GetHotelById(id int) (dto.HotelDto, error) {
+
+	var hotel model.Hotel = client.GetHotelById(id)
+	var hotelDto dto.HotelDto
+
+	if hotel.Id == 0 {
+		return hotelDto, errors.New("hotel not found")
+	}
+	hotelDto.Id = hotel.Id
+	hotelDto.Name = hotel.Name
+	hotelDto.RoomAmount = hotel.RoomAmount
+	hotelDto.Description = hotel.Description
+	hotelDto.StreetName = hotel.StreetName
+	hotelDto.StreetNumber = hotel.StreetNumberNumber
+
+	return hotelDto, nil
 }
